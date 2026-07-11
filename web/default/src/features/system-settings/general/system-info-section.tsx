ca4b93ed7be8186192b27475c862c0e0a5f79a45
Wei yang <refeiner001@qq.com>
2026-07-11 22:12:05 +0800

    feat: 更新系统设置页面

--- web/default/src/features/system-settings/general/system-info-section.tsx
+++ web/default/src/features/system-settings/general/system-info-section.tsx
@@ -242,7 +242,7 @@ export function SystemInfoSection({ defaultValues }: SystemInfoSectionProps) {
                   <FormItem>
                     <FormLabel>{t('System Name')}</FormLabel>
                     <FormControl>
-                      <Input placeholder={t('New API')} {...field} />
+                      <Input placeholder='TokenAI' {...field} />
                     </FormControl>
                     <FormDescription>
                       {t('The name displayed across the application')}
